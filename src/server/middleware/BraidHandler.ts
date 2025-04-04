c9e87a1a15280201686e0be718c2ce450fb676a1
thhck <thhck@proton.me>
2025-04-04 14:57:59 +0200

    disabling multiplexing

--- src/server/middleware/BraidHandler.ts
+++ src/server/middleware/BraidHandler.ts
@@ -8,15 +8,17 @@ import { http_server as braidify } from 'braid-http'
 * TODO
  */
 export class BraidHandler extends HttpHandler {
-
+    private readonly braidify: any;
   public constructor() {
     super();
+    this.braidify = braidify
+    this.braidify.enable_multiplex = false
   }
 
   public async handle(input: HttpHandlerInput): Promise<void> {
     return new Promise((resolve): void => {
       // this.braidHandler(input.request, input.response, (): void => resolve());
-      braidify(input.request, input.response, (): void => resolve());
+      this.braidify(input.request, input.response, (): void => resolve());
     });
   }
 }
